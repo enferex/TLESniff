3556ba44b4c99ed29415613f13ff296b586dbbe7
Matt Davis <mattdavis9@gmail.com>
2017-07-22 19:38:20 -0700

    Report if no data sites are listed in Net.hs

--- Main.hs
+++ Main.hs
@@ -1,4 +1,5 @@
 module Main where
+import System.Exit
 import TLE.Base
 import TLE.Net
 import TLE.Database
@@ -11,6 +12,9 @@ test = do
 main = do
     sites <- downloadData
     --sites <- test
+    if (length sites) == 0
+    then putStrLn "[-] No sites specified in Net.hs... add some!" >> exitFailure
+    else return ()
     let tles = concat $ map (buildTLEs . content) sites
     putStrLn $ "[+] Constructed " ++ (show . length) tles ++ " TLE entries."
     putStrLn $ "[+] From " ++ show ((length tles) * 3) ++ " source lines."
