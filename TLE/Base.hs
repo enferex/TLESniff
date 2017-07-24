81e4bee8278c5733cb53be8523c82d7752b3dfa3
Matt Davis <mattdavis9@gmail.com>
2017-07-23 22:37:11 -0700

    Move some lines over

--- TLE/Base.hs
+++ TLE/Base.hs
@@ -36,32 +36,33 @@ fltAss a b c
 fltAss' a b c = read $ "0." ++ subStr a b c :: Double
 
 buildTLE :: String -> String -> String -> TLE
-buildTLE name l1 l2 = [ -- TLE Line 0
-                        TLEField "name"           $ S (trim name)
-                        -- TLE Line 1
-                      , TLEField "satNo"          $ I (intFrom 2 6 l1)
-                      , TLEField "classification" $ C (l1 !! 7)
-                      , TLEField "launchYear"     $ I (intFrom 9  10 l1)
-                      , TLEField "launchNumber"   $ I (intFrom 11 13 l1)
-                      , TLEField "launchPiece"    $ S (trim $ take 3 $ drop 14 l1)
-                      , TLEField "epochYear"      $ I (intFrom 18 19 l1)
-                      , TLEField "epochDay"       $ D (fltFrom 20 31 l1)
-                      , TLEField "fstMeanMotion"  $ D (fltFrom 33 42 l1)
-                      , TLEField "sndMeanMotion"  $ D (fltAss 44 51  l1)
-                      , TLEField "bstarDrag"      $ D (fltAss 53 60  l1)
-                      , TLEField "ephermisType"   $ I (intFrom 62 62 l1)
-                      , TLEField "elementNo"      $ I (intFrom 64 67 l1)
-                      , TLEField "checksumLine1"  $ I (intFrom 68 68 l1)
-                      -- TLE Line 2
-                      , TLEField "catNumber"      $ I (intFrom 2 6   l2)
-                      , TLEField "orbInclination" $ D (fltFrom 8 15  l2)
-                      , TLEField "rightAscension" $ D (fltFrom 17 24 l2)
-                      , TLEField "eccentricity"   $ D (fltAss' 26 32 l2)
-                      , TLEField "argPerigee"     $ D (fltFrom 34 41 l2)
-                      , TLEField "meanAnomaly"    $ D (fltFrom 43 50 l2)
-                      , TLEField "meanMotion"     $ D (fltFrom 52 62 l2)
-                      , TLEField "revNumEpoch"    $ I (intFrom 63 67 l2)
-                      , TLEField "checksumLine2"  $ I (intFrom 68 68 l2) ]
+buildTLE name l1 l2 = [
+    -- TLE Line 0
+    TLEField "name"           $ S (trim name)
+    -- TLE Line 1
+    , TLEField "satNo"          $ I (intFrom 2 6 l1)
+    , TLEField "classification" $ C (l1 !! 7)
+    , TLEField "launchYear"     $ I (intFrom 9  10 l1)
+    , TLEField "launchNumber"   $ I (intFrom 11 13 l1)
+    , TLEField "launchPiece"    $ S (trim $ take 3 $ drop 14 l1)
+    , TLEField "epochYear"      $ I (intFrom 18 19 l1)
+    , TLEField "epochDay"       $ D (fltFrom 20 31 l1)
+    , TLEField "fstMeanMotion"  $ D (fltFrom 33 42 l1)
+    , TLEField "sndMeanMotion"  $ D (fltAss 44 51  l1)
+    , TLEField "bstarDrag"      $ D (fltAss 53 60  l1)
+    , TLEField "ephermisType"   $ I (intFrom 62 62 l1)
+    , TLEField "elementNo"      $ I (intFrom 64 67 l1)
+    , TLEField "checksumLine1"  $ I (intFrom 68 68 l1)
+    -- TLE Line 2
+    , TLEField "catNumber"      $ I (intFrom 2 6   l2)
+    , TLEField "orbInclination" $ D (fltFrom 8 15  l2)
+    , TLEField "rightAscension" $ D (fltFrom 17 24 l2)
+    , TLEField "eccentricity"   $ D (fltAss' 26 32 l2)
+    , TLEField "argPerigee"     $ D (fltFrom 34 41 l2)
+    , TLEField "meanAnomaly"    $ D (fltFrom 43 50 l2)
+    , TLEField "meanMotion"     $ D (fltFrom 52 62 l2)
+    , TLEField "revNumEpoch"    $ I (intFrom 63 67 l2)
+    , TLEField "checksumLine2"  $ I (intFrom 68 68 l2) ]
 
 buildTLEs' :: [String] -> [TLE] -> [TLE]
 buildTLEs' (n:l1:l2:xs) ts = buildTLEs' xs ((buildTLE n l1 l2) : ts)
